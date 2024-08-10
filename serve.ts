55a311e98625e56dfe4e24b136b1f7063463072f
Thawat-2061 <65011212061@msu.ac.th>
2024-08-10 23:38:01 +0700

    Initial commit

--- serve.ts
+++ serve.ts
@@ -5,6 +5,6 @@ const port = process.env.port || 3000;
 const server = http.createServer(app);
 
 server.listen(port,()=>{
-    console.log("Server Start");
+    console.log("Hello Lotto777");
     
 });
