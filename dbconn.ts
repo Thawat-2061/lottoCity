0893b523ff1e4ac6de898573711292386b5709ab
Thawat-2061 <65011212061@msu.ac.th>
2024-09-07 01:04:37 +0700

    newne

--- dbconn.ts
+++ dbconn.ts
@@ -1,11 +1,8 @@
 import mysql from "mysql";
 
 export const conn = mysql.createPool({
-  // connectionLimit: 10,
-  // host: "localhost",
-  // user: "demo",
-  // password: "abc123",
-  // database: "test",
+
+
   connectionLimit: 10,
   host: "202.28.34.197",
   user: "web66_65011212061",
