a31654e602ee668108c4328d465c08fbbfb73e3d
Thawat-2061 <65011212061@msu.ac.th>
2024-09-07 20:49:47 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,33 +1,26 @@
-import  express  from "express";
+import express from "express";
 import { router as people } from "./api/people";
 import bodyParser from "body-parser";
 import { router as movie } from "./api/movies";
-import { router as  stars} from "./api/stars";
+import { router as stars } from "./api/stars";
 import { router as creators } from "./api/creators";
 import { router as login } from "./api/login";
 
 export const app = express();
 
-
-
 const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.text());
 app.use(bodyParser.json());
-app.use("/",(req,res)=>{
-    res.send("Hello World!")
+
+// Register your routes first
+app.use("/people", people);
+app.use("/movie", movie);
+app.use("/stars", stars);
+app.use("/creators", creators);
+app.use("/login", login);
+
+// The root route should be the last
+app.use("/", (req, res) => {
+  res.send("Hello World!");
 });
-
-app.use("/people",people);
-app.use("/movie",movie);
-app.use("/stars",stars);
-app.use("/creators",creators);
-
-app.use("/login",login);
-
-
-
-
-
-
-
