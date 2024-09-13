82165b983d8fb827dc1b754fbab29b073a6f2559
Thawat-2061 <65011212061@msu.ac.th>
2024-09-13 22:11:34 +0700

    API

--- dbconn.ts
+++ dbconn.ts
@@ -4,10 +4,16 @@ export const conn = mysql.createPool({
 
 
   connectionLimit: 10,
-  host: "sql12.freemysqlhosting.net",
-  user: "sql12731231",
-  password: "T6FHLLJQNh",
-  database: "sql12731231",
+  host: "202.28.34.197",
+  user: "web66_65011212061",
+  password: "65011212061@csmsu",
+  database: "web66_65011212061",
+
+  // connectionLimit: 10,
+  // host: "sql12.freemysqlhosting.net",
+  // user: "sql12731231",
+  // password: "T6FHLLJQNh",
+  // database: "sql12731231",
 
 });
 
